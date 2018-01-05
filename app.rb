8db07cda406f368712cd53b0dd793745d8f654ae
Steven Senkowski <scsenkowski@gmail.com>
2018-01-05 02:11:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,6 +31,17 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
+    if condition
+
+    elsif condition
+
+    elsif condition
+
+    elsif
+
+    else
+
+    end
     "#{@num1}#{@operation}#{@num2}"
   end
 
