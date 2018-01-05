08c8d91510c41c2c876cb572a7703cdfebf1872b
Steven Senkowski <scsenkowski@gmail.com>
2018-01-05 01:55:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,6 +17,10 @@ class App < Sinatra::Base
     "#{@phrase}"
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @sentence = "#{params[:word1] params[:word2] params[:word3] params[:word4]params[:word5]}"
+  end
+
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
     @num1 = params[:number1]
